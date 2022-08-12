324e85763f230cf26547129e5f00d88df2171c18
Andre Alves Araujo <andrealvesaraujo.1995@gmail.com>
2022-08-11 21:16:46 -0300

    Criando pagina cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,13 +1,38 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png'
 
 function SignUp() {
-  
-    return (
-      <div>
-        <h1>
-          Pagina de Cadastro
-        </h1>
+
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('clicou')
+  }
+
+  return (
+    <div className='container-center'>
+      <div className='login'>
+        <div className='login-area'>
+          <img src={logo} alt="Sistema logo"/>
+        </div>
+        
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input type='text' placeholder='Seu nome' value={nome} onChange={(e)=> setNome(e.target.value)} />
+          <input type='text' placeholder='email@email.com' value={email} onChange={(e)=> setEmail(e.target.value)} />
+          <input type='password' placeholder='********' value={password} onChange={(e)=> setPassword(e.target.value)} />
+
+          <button type='submit'>Cadastrar</button>
+        </form>
+
+        <Link to='/'>Já tem uma conta? Entre</Link>
       </div>
-    );
+    </div>
+  );
 }
   
 export default SignUp;
