8b9fe6a12b7796c5320d1e8ba04c7ff04db41d84
Andre Alves Araujo <andrealvesaraujo.1995@gmail.com>
2022-08-11 22:37:21 -0300

    Criando alertas

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,5 +1,7 @@
 import {useState, createContext, useEffect} from 'react'
 import firebase from '../services/firebaseConnection'
+import { toast } from 'react-toastify';
+
 
 export const AuthContext = createContext({})
 
@@ -44,11 +46,15 @@ function AuthProvider({children}){
             setUser(data)
             storageUser(data)
             setLoadingAuth(false)
+            toast.success('Bem vindo de volta')
+
 
         })
         .catch((error)=>{
             console.log(error)
+            toast.error('Ops algo deu errado!')
             setLoadingAuth(false)
+
         })
     }
 
@@ -74,12 +80,16 @@ function AuthProvider({children}){
                 setUser(data)
                 storageUser(data)
                 setLoadingAuth(false)
+                toast.success('Bem vindo a plataforma')
+
 
             })
         })
         .catch((error)=>{
             console.log(error)
+            toast.error('Ops algo deu errado!')
             setLoadingAuth(false)
+
         })
     }
 
