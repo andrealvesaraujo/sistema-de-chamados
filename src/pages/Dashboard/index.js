ee1c0130d6def1ba30ebfeea1c85c3011cd98cc8
Andre Alves Araujo <andrealvesaraujo.1995@gmail.com>
2022-08-12 18:03:06 -0300

    Criando Header

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,12 +2,15 @@ import {useContext} from 'react'
 
 import { AuthContext } from '../../contexts/auth'
 
+import Header from '../../components/Header'
+
 export default function Dashboard() {
   
   const {signOut} = useContext(AuthContext)
   
   return (
     <div>
+        <Header/>
         <h1>Pagina Dashboard</h1>
         <button onClick={()=> signOut()}>Fazer logout</button>
     </div>
