297a5c52989109e36c3e30aeb0b63d9fe0cf5a99
Andre Alves Araujo <andrealvesaraujo.1995@gmail.com>
2022-08-15 17:22:21 -0300

    Registrando chamado

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,5 @@
-import { useState, useEffect, useContext } from 'react'
 import firebase from '../../services/firebaseConnection'
+import { useState, useEffect, useContext } from 'react'
 
 import {AuthContext} from '../../contexts/auth'
 
@@ -9,6 +9,7 @@ import Header from '../../components/Header'
 import Title from '../../components/Title'
 
 import {FiPlusCircle} from 'react-icons/fi'
+import { toast } from 'react-toastify'
 
 export default function New() {
  
@@ -61,8 +62,30 @@ export default function New() {
     }, [])
 
 
-    function handleRegister(e){
+    async function handleRegister(e){
         e.preventDefault()
+
+        await firebase.firestore().collection('chamados')
+        .add({
+            created: new Date(),
+            cliente: customers[customerSelected].nomeFantasia,
+            clienteId: customers[customerSelected].id,
+            assunto: assunto,
+            status: status,
+            complemento: complemento,
+            userId: user.uid
+        })
+        .then(()=>{
+            toast.success('Chamado criado com sucesso')
+            setComplemento('');
+            setCustomerSelected(0)
+        })
+        .catch((error)=>{
+            toast.error('Ops, erro ao registrar, tente mais tarde')
+            console.log(error)
+        })
+
+
     }
     
 
@@ -122,7 +145,7 @@ export default function New() {
                         </div>
 
                         <label>Complemento</label>
-                        <textarea type="text" placeholder='Descreva seu problema(opcional)' value={complemento} onChange={(e)=> setComplemento(e.target.value)} />
+                        <textarea type="text" placeholder='Descreva seu problema (opcional)' value={complemento} onChange={(e)=> setComplemento(e.target.value)} />
 
                         <button type='submit'>Registrar</button>
                     </form>
